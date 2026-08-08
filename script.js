d18ac97f33e4dbc2f34d8e9febd9a14d06792410
kingmusa124 <gabrielmusa775@gmail.com>
2026-08-08 20:03:53 +0100

    Added mockup dashboard for BITDA presentation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+console.log("%cBuilt with 💻 and ⚡ by Musa Gabriel Gadzama - NIEEESA UNIMAID", "color: #002147; font-size: 14px; font-weight: bold; background: #f4f7f9; padding: 10px; border-radius: 5px; border: 1px solid #002147;");
+//
+//====================================================================
+  // NIEEESA UNIMAID - OFFICIAL STUDENT PORTAL
+  // System Architecture & Frontend Development by Musa Gabriel Gadzama
+  // Built for the Department of Electrical and Electronics Engineering
+  // Session: 2025/2026
+//==================================================================== 
 // Wait for the webpage to fully load
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
